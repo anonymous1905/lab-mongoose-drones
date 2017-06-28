80daa0d9b750b14d9702a563e205957a0dce4071
Anthony Miclet <micletanthony@gmail.com>
2017-06-28 09:08:42 -0400

    drone project done

--- starter-code/bin/seeds.js
+++ starter-code/bin/seeds.js
@@ -1,1 +1,39 @@
-// Iteration #1
+const mongoose = require('mongoose');
+
+
+mongoose.connect('mongodb://localhost/drones-dev');
+
+const DroneModel = require('../models/drone.js');
+
+const droneArray = [
+  {
+    droneName: "Destructor",
+    propellers: 12,
+    maxSpeed: 300,
+  },
+  {
+    droneName: "Stealthor",
+    propellers: 100,
+    maxSpeed: 2000,
+  },
+  {
+    droneName: "Raptor",
+    propellers: 40,
+    maxSpeed: 600,
+  },
+];
+
+DroneModel.create(
+  droneArray,
+
+  (err, droneResults) =>{
+    if (err) {
+      console.log('OMG! Database ERROR');
+      return;
+    }
+droneResults.forEach((oneDrone)=> {
+  console.log('New Drone'+ oneDrone.droneName);
+  mongoose.disconnect();
+});
+}
+  );
